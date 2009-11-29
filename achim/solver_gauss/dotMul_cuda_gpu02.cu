6a442dda11f4c3153cb794106d917500ac044a5c
achim.grolms <achim.grolms@39c99c64-b8d6-11de-b52b-63f31fbada87>
2009-11-29 12:11:07 +0000

    importet Buyus's code, as after saturdays changes

---
+++ achim/solver_gauss/dotMul_cuda_gpu02.cu
@@ -0,0 +1,39 @@
+#include "cuda.h"
+#include <stdio.h>
+#include "projektcuda.h"
+#include "project_comm.h"
+//#include "mex.h"
+/* Kernel to square elements of the array on the GPU */
+
+__global__ void device_dotMul(t_ve* in1, t_ve* in2,t_ve* out, unsigned int N)
+{
+	__shared__ float Cs[VECTOR_BLOCK_SIZE];
+	int idx = blockIdx.x*blockDim.x+threadIdx.x;
+	
+	Cs[threadIdx.x] = 0;
+
+	if ( idx < N ) {
+	    Cs[threadIdx.x] = in1[ idx ] * in2[ idx ];
+	}
+
+	t_ve blocksum = 0;
+	
+	if(threadIdx.x==0){
+		out[blockIdx.x]=0;
+	}
+	__syncthreads();
+	
+	if(threadIdx.x==0){
+	    for ( int i = 0; i < blockDim.x; i++ ) {
+		     blocksum += Cs[i];
+		}
+		out[blockIdx.x]=blocksum;
+	}
+	__syncthreads();
+	if ( idx == 0 ) {
+	     for ( int i = 1; i < gridDim.x; i++ ) {
+		     out[0] += out[i];
+		 }
+	}
+
+}
