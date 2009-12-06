fe7eb143905d32ccfa68bc9a20eba5d29586dc97
achim.grolms <achim.grolms@39c99c64-b8d6-11de-b52b-63f31fbada87>
2009-12-06 23:31:43 +0000

    correct working version of dotmul

--- achim/solver_gauss/dotMul_cuda_gpu02.cu
+++ achim/solver_gauss/dotMul_cuda_gpu02.cu
@@ -1,8 +1,11 @@
 
 #include "projektcuda.h"
 
+#include <stdio.h>
+#include <device_functions.h>
 
-
+__device__ unsigned int count = 0;
+__shared__ bool isLastBlockDone;
 __global__ void device_dotMul(t_ve* in1, t_ve* in2,t_ve* out, unsigned int N)
 {
 	__shared__ float Cs[512];
@@ -16,23 +19,26 @@ __global__ void device_dotMul(t_ve* in1, t_ve* in2,t_ve* out, unsigned int N)
 
 	t_ve blocksum = 0;
 
-	if(threadIdx.x==0){
-		out[blockIdx.x]=0;
-	}
 	__syncthreads();
 
 	if(threadIdx.x==0){
 	    for ( int i = 0; i < blockDim.x; i++ ) {
 		     blocksum += Cs[i];
 		}
-		out[blockIdx.x]=blocksum;
-	}
-	__syncthreads();
-	if ( idx == 0 ) {
-	     for ( int i = 1; i < gridDim.x; i++ ) {
-		     out[0] += out[i];
-		 }
-	}
-
+		out[blockIdx.x]=blocksum ;
+    }
+     __threadfence();
+    if(threadIdx.x==0){
+        unsigned int value = atomicInc( &count, gridDim.x );
+        isLastBlockDone = ( value == ( gridDim.x -1 ) );
+    }
+    __syncthreads();
+    if(threadIdx.x==0){
+         if ( isLastBlockDone ) {
+             for ( int i = 1; i < gridDim.x; i++ ) {
+                 out[0] += out[i];
+             }
+        }
+    }
 }
 
