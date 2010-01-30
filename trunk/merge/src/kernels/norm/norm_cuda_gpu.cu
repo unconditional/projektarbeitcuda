6df251d6b3f0afbe2c15a5ba5700bca1dab4982c
achim.grolms <achim.grolms@39c99c64-b8d6-11de-b52b-63f31fbada87>
2010-01-30 18:03:18 +0000

    cleaned up code, added auto-checker for norm

--- trunk/merge/src/kernels/norm/norm_cuda_gpu.cu
+++ trunk/merge/src/kernels/norm/norm_cuda_gpu.cu
@@ -72,3 +72,46 @@ __global__ void kernel_norm(t_ve* in,t_ve* out )
 
 }
 
+
+__host__ void dbg_norm_checkresult ( t_ve *in1,
+
+                                     t_ve tobeckecked,
+                                     t_mindex N ,
+                                     char* debugname
+                                      )
+
+                          {
+
+    cudaError_t e;
+
+
+    t_ve* v1 = (t_ve*) malloc( sizeof( t_ve* ) * N );
+    if (  v1 == NULL ) { fprintf(stderr, "sorry, can not allocate memory for you C"); exit( -1 ); }
+
+
+
+    e = cudaMemcpy( v1, in1, sizeof(t_ve) * N , cudaMemcpyDeviceToHost);
+    CUDA_UTIL_ERRORCHECK(" cudaMemcpy debugbuffer");
+
+
+    t_ve calresult = 0;
+
+    for( t_mindex i = 0; i < N; i++ ) {
+        calresult += v1[i] * v1[i];
+    }
+   t_ve calnorm = sqrt(calresult);
+
+    if ( abs( calnorm - tobeckecked ) < 0.001 ) {
+        printf("\n Norm %s OK", debugname );
+    }
+    else {
+        printf("\n Norm %s *not* OK :  expected %f, got %f", debugname , calresult, tobeckecked );
+        exit( - 1 );
+    }
+
+    free(v1);
+
+
+}
+
+
