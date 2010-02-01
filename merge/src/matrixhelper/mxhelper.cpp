08a680394c3ae2906af2e7c43d59bcb940f51939
achim.grolms <achim.grolms@39c99c64-b8d6-11de-b52b-63f31fbada87>
2010-02-01 21:04:56 +0000

    Configuring new problem-Matrix

--- merge/src/matrixhelper/mxhelper.cpp
+++ merge/src/matrixhelper/mxhelper.cpp
@@ -24,6 +24,56 @@
 
 
 
+void genmtx_t2(  t_SparseMatrix* sm_in, t_mindex N  ) {
+
+    sm_in->m = N;
+    sm_in->n = N;
+
+    t_mindex cnt_elements = 3 * N - 2 - 2;
+    sm_in->nzmax = cnt_elements;
+
+    sm_in->pRow = ( t_mindex* ) malloc( sizeof( t_mindex ) * ( N + 1) );
+    if ( sm_in->pRow == NULL) { fprintf(stderr, "sorry, can not allocate memory for you  a.pRow"); exit( -1 ); }
+
+    sm_in->pNZElement = ( t_ve* ) malloc( sizeof( t_ve ) * cnt_elements );
+    if ( sm_in->pNZElement == NULL) { fprintf(stderr, "sorry, can not allocate memory for you  a.pNZElement"); exit( -1 ); }
+
+    sm_in->pCol = ( t_mindex* ) malloc( sizeof( t_mindex ) * cnt_elements );
+    if ( sm_in->pCol == NULL) { fprintf(stderr, "sorry, can not allocate memory for you a.pCol"); exit( -1 ); }
+
+    t_mindex i = 0;
+    t_mindex row ;
+    for ( row = 0; row < N; row++ ) {
+        sm_in->pRow[row] = i;
+
+
+
+        /* lower diag */
+        if ( ( row > 0 ) && ( row < N -1 ) ) {
+            sm_in->pNZElement[i] = -1;
+            sm_in->pCol[i]   = row - 1;
+            i++;
+        }
+        /* main diag */
+        sm_in->pNZElement[i] = 2;
+        sm_in->pCol[i]   = row;
+        i++;
+
+        /* upper diag */
+        if ( ( row < N - 1 ) && ( row > 0 ) ) {
+            sm_in->pNZElement[i] = -1;
+            sm_in->pCol[i]   = row + 1;
+            i++;
+        }
+
+    }
+    sm_in->pRow[row] = i;
+
+    printf("\n *** unimplemented *** \n ");
+}
+
+
+
 void genmtx_t1(  t_SparseMatrix* sm_in, t_mindex N  ) {
 
     sm_in->m = N;
