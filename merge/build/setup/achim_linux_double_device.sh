7b89638b30fc77bdc5bbaae86ea0c409c2f5e9f1
achim.grolms <achim.grolms@39c99c64-b8d6-11de-b52b-63f31fbada87>
2010-02-01 23:54:39 +0000

    prepared the tesla skript

---
+++ merge/build/setup/achim_linux_double_device.sh
@@ -0,0 +1,26 @@
+#! /bin/sh
+
+DOUBLEDEFINE="-DPRJACUDADOUBLE"
+
+export DOUBLEDEFINE
+
+DOUBLEFLAGS="$DOUBLEDEFINE -arch=sm_13" # as describe in https://www.cs.virginia.edu/~csadmin/wiki/index.php/CUDA_Support/Enabling_double-precision
+
+
+PRJACUDACFLAGS="-I include/common -I include $DOUBLEFLAGS"
+export PRJACUDACFLAGS
+
+PRJACUDAOBJEXT=o
+export PRJACUDAOBJEXT
+
+PRJACUDAEXEEXT=exe
+export PRJACUDAEXEEXT
+
+PRJACUDAHOSTLD=gcc
+export PRJACUDAHOSTLD
+
+PRJACUDALIBEXT=a
+export PRJACUDALIBEXT
+
+PRJACUDAHOSCC=gcc
+export PRJACUDAHOSCC
