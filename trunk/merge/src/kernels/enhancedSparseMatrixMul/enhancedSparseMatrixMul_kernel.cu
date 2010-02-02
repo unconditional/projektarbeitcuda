29abc731a6dc786b05b1b8369dea1359e831e0e7
achim.grolms <achim.grolms@39c99c64-b8d6-11de-b52b-63f31fbada87>
2010-02-02 21:30:23 +0000

    removed hard-coded floats...

--- trunk/merge/src/kernels/enhancedSparseMatrixMul/enhancedSparseMatrixMul_kernel.cu
+++ trunk/merge/src/kernels/enhancedSparseMatrixMul/enhancedSparseMatrixMul_kernel.cu
@@ -1,22 +1,21 @@
-#include "cuda.h"
 #include <stdio.h>
 #include "projektcuda.h"
-#include "project_comm.h"
+
 
 /* Kernel to square elements of the array on the GPU */
 /*
-	input Matrix pSparseMatrix  ,input Vector pVector 
-	Vector pResultVector output vector 
+	input Matrix pSparseMatrix  ,input Vector pVector
+	Vector pResultVector output vector
 	C=A*B
 description:
-	each row of A occuppy one block. if gridDim is smaller than the row number of A  
+	each row of A occuppy one block. if gridDim is smaller than the row number of A
 Release:
 	enhancedSparseMatrixMul_kernel.cu
 */
 __global__ void enhancedSparseMatrixMul(t_FullMatrix pResultVector,t_SparseMatrix pSparseMatrix, t_FullMatrix pVector,t_ve n)
 {
-	__shared__ float Cs[VECTOR_BLOCK_SIZE];//VECTOR_BLOCK_SIZE shuld equal blockDim 512
-	//define gridIndex, if gridDim < mA, gridIndex > 0; 
+	__shared__ t_ve Cs[VECTOR_BLOCK_SIZE];//VECTOR_BLOCK_SIZE shuld equal blockDim 512
+	//define gridIndex, if gridDim < mA, gridIndex > 0;
 	int gridIndex = 0;
 	//int idx = gridIndex*gridDim.x + blockIdx.x*blockDim.x+threadIdx.x;
     t_ve *pMatrixElements, *pVectorElements, *pResultElements;
@@ -39,7 +38,7 @@ __global__ void enhancedSparseMatrixMul(t_FullMatrix pResultVector,t_SparseMatri
     if(m != pResultVector.m*(pResultVector.n)){
         //printf("Result Vector does not match the Matrix\n");
         return;
-    }   
+    }
     if(n != pVector.m*(pVector.n)){
         //printf("input Vector does not match the Matrix\n");
         return;
@@ -53,7 +52,7 @@ __global__ void enhancedSparseMatrixMul(t_FullMatrix pResultVector,t_SparseMatri
 			pResultElements[blockIdx.x]=0;
 		//C[gridIndex*gridDim.x+blockIdx.x]=0;
 		}
-		//following is operations within one block 
+		//following is operations within one block
 		// initialize the dot product for each row in A and vector B
 		t_ve blocksum = 0;
 		//if nB> blockDim, split repeat the
@@ -65,28 +64,28 @@ __global__ void enhancedSparseMatrixMul(t_FullMatrix pResultVector,t_SparseMatri
 			__syncthreads();
 
 			if (( (gridIndex*gridDim.x+blockIdx.x)<aEnd)&&((b+threadIdx.x) < bEnd)) {
-				
+
 				Cs[threadIdx.x] = n*pMatrixElements[b + threadIdx.x] * pVectorElements[pCol[b + threadIdx.x ]];
 			}
 			__syncthreads();
-				
+
 			if(threadIdx.x == 0){
 				int kEnd = bEnd-b;
 				if(kEnd > VECTOR_BLOCK_SIZE)kEnd = VECTOR_BLOCK_SIZE;
-				//Because I add Cs[0...k], if blockSize and Matrix does not fit, Parts of Cs[k] are not initialized as 0.  		
+				//Because I add Cs[0...k], if blockSize and Matrix does not fit, Parts of Cs[k] are not initialized as 0.
 				for (int k = 0; k < kEnd; k++) blocksum += Cs[k];
 				//blocksum = 2;
-			
+
 			}
 			__syncthreads();
-			
+
 			//Cs[threadIdx.x] = 0;
-			//__syncthreads();	
+			//__syncthreads();
 		}//for b
 		__syncthreads();
 
 		if(threadIdx.x == 0) pResultElements[blockIdx.x] = blocksum;//?????????????
-		__syncthreads();	
-    
- 
+		__syncthreads();
+
+
 }
