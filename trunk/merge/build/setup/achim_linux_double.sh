0ea392e6cc8af8fd04775b12200125ed0b1a9632
achim.grolms <achim.grolms@39c99c64-b8d6-11de-b52b-63f31fbada87>
2010-02-03 00:38:55 +0000

    added Wall to copiler flags (shows ugly warnings of cude-headerfile :-/

--- trunk/merge/build/setup/achim_linux_double.sh
+++ trunk/merge/build/setup/achim_linux_double.sh
@@ -7,7 +7,7 @@ export DOUBLEDEFINE
 DOUBLEFLAGS="$DOUBLEDEFINE -arch=sm_13" # as describe in https://www.cs.virginia.edu/~csadmin/wiki/index.php/CUDA_Support/Enabling_double-precision
 
 
-PRJACUDACFLAGS="-deviceemu -DPRJCUDAEMU -I include/common -I include $DOUBLEFLAGS"
+PRJACUDACFLAGS="-Xcompiler \"-Wall\" -deviceemu -DPRJCUDAEMU -I include/common -I include $DOUBLEFLAGS"
 export PRJACUDACFLAGS
 
 PRJACUDAOBJEXT=o
